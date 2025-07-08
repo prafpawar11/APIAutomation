2eeca6116891b1888c404b18b1a9e64353038f33
prafulp1010 <prafulp1010@gmail.com>
2025-07-08 08:28:35 +0530

    Update RunnerTest.java

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features= {"src/test/java/FeatureFiles"},
+		features= {"src/test/java/FeatureFiles/customer.feature"},
 		glue= {"StepApi"},
 		monochrome=true,
 		dryRun=false
